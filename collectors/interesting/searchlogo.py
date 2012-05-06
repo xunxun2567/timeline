88655cc2ab09944720d5b1d8dda25a8173719331
konglingkai <konglingkai@egibbon.com>
2012-05-06 13:28:32 +0800

    go

---
+++ collectors/interesting/searchlogo.py
@@ -0,0 +1,53 @@
+import urllib2
+import datetime
+from lxml import etree
+from kernel import collector
+
+LIST_URL_B = 'http://www.baidu.com/'
+LIST_XPATH_B = '//*[@id="lg"]'
+URL_PATH_B = 'img'
+TITLE_PATH_B ='map/area'
+
+LIST_URL_G = 'http://www.google.com/'
+LIST_XPATH_G = '//*[@id="hplogo"]'
+URL_PATH_G = 'img'
+
+class BaiduLogoCollector(collector.Collector):
+    def fetch(self):
+        print "Start cloning logo url from baidu.com..."
+        time = datetime.datetime.now().date().strftime('%Y-%m-%d')
+        parser = etree.HTMLParser(encoding='gb2312')
+        text = urllib2.urlopen(LIST_URL_B).read(-1)
+        tree = etree.HTML(text, parser=parser)
+
+        nodes = tree.xpath(LIST_XPATH_B)
+        for node in nodes:
+            node1 = node.find(URL_PATH_B)
+            url = node1.attrib['src']
+            node2 = node.find(TITLE_PATH_B)
+            title = node2.attrib['title']
+            print "%s: %s - %s" % (time, title, url)
+            collector.object_found.send(self, time=time, title=title, url=url)
+
+class GoogleLogoCollector(collector.Collector):
+    def fetch(self):
+        print "Start cloning logo url from google.com..."
+        time = datetime.datetime.now().date().strftime('%Y-%m-%d')
+        parser = etree.HTMLParser(encoding='Big5')
+        text = urllib2.urlopen('http://www.google.com').read(-1)
+        print text
+        tree = etree.HTML(text, parser=parser)
+
+        nodes = tree.xpath(LIST_XPATH_G)
+        url=''
+        for node in nodes:
+            for attr in node.attrib:
+                if attr == 'src':
+                    url = node.attrib['src']
+                    url = LIST_URL_G + url[1:len(url)-1]
+                if attr == 'style':
+                    url = node.attrib['style']
+                    url = LIST_URL_G + url[len('background:url(/'):url.index(')')]
+        title = node.attrib['title']
+        print "%s: %s - %s" % (time, title, url)
+        collector.object_found.send(self, time=time, title=title, url=url)
---
+++ collectors/weather/shanghai.py
@@ -0,0 +1,71 @@
+# .-. coding=utf-8
+import urllib2
+import datetime
+from lxml import etree
+from kernel import collector
+
+LIST_URL = 'http://www.weather.com.cn/weather/101020100.shtml'
+
+LIST_XPATH = '//*[@id="7d"]/div/table[2]'
+DATE_PATH = 'tr/td/a'
+DAYWEA_PATH = 'tr/td[4]/a'
+NIGHTWEA_PATH='tr[2]/td[3]/a'
+HIGHTEMP_PATH='tr/td[5]/a/span/strong'
+LOWTEMP_PATH='tr[2]/td[4]/a/span/strong'
+
+def print_dict(dict):
+    for key in dict:
+        print u"%s:%s" % (key, dict[key])
+
+class ShanghaiCollecotr(collector.Collector):
+    def fetch(self):
+        time = datetime.datetime.now().date().strftime('%Y-%m-%d')
+        print "Start cloning from www.weather.com.cn..."
+        parser = etree.HTMLParser(encoding='utf-8')
+        text = urllib2.urlopen(LIST_URL).read(-1)
+        tree = etree.HTML(text, parser=parser)
+
+        weaDict = {}
+        nodes = tree.xpath(LIST_XPATH)
+        for node in nodes:
+            node1 = node.find(DATE_PATH)
+            dateDay = node1.text
+            dateDay = dateDay[0:dateDay.index(u'日')]
+            curDay = datetime.datetime.now().date().day
+            if int(dateDay) != int(curDay):
+                print u"错误！天气预报日期%s日不是当前日期%s日" % (dateDay, curDay)
+                return
+            node2 = node.find(DAYWEA_PATH)
+            #print node2.text
+            node3 = node.find(HIGHTEMP_PATH)
+            #print node3.text
+            curHour = datetime.datetime.now().time().hour
+            if int(curHour) >= 18 or int(curHour) <= 8 :
+                weaDict[u"夜间"] = node2.text
+                weaDict[u"低温"] = node3.text
+                print u"现在时间%s:00,已超过18:00，只能取夜间天气" % curHour
+                print time
+                print_dict(weaDict)
+                return
+            weaDict[u"白天"] = node2.text
+            weaDict[u"高温"] = node3.text
+            node4 = node.find(NIGHTWEA_PATH)
+            weaDict[u"夜间"] = node4.text
+            node5 = node.find(LOWTEMP_PATH)
+            weaDict[u"低温"] = node5.text
+
+            print time
+            print_dict(weaDict)
+            #collector.object_found.send(self, time=time, title=title, url=url)
+"""
+        for node in nodes:
+                node1 = node.find(TITLE_PATH)
+                title = node1.text
+                url = node1.attrib['href']
+                node2 = node.find(DATE_PATH)
+                #datetime.strptime - can you please work with unicode???
+                time = node2.text.replace(u'年', '-').replace(u'月', '-').replace(u'日', '-')
+                time = datetime.datetime.strptime(time, '%Y-%m-%d-')
+                print "%s: %s - %s" % (time, title, url)
+
+      """
